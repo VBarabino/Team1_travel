150ee7ba07dd2253439d8d2bf36813b05d8a70c5
Vlad Grigoriu <vlad.grigoriu4@gmail.com>
2020-11-26 13:02:22 +0100

    fetch-context-login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,103 @@
-import logo from './logo.svg';
+import React,{ useState , useEffect } from 'react'
 import './App.css';
+import './index.css';
+import LoginForm from './Components/LoginForm';
+import HeaderSito from './Components/HeaderSito'
+import DataContextProvider from './Context/DataContext';
+
+
+
+
+
 
 function App() {
+  const adminUser = {
+    email:"team1@team1.com",
+    password:"team1"
+  }
+  const [ user , setUser ] = useState({name:"",email:""})
+  const [ error , setError ] = useState("")
+ 
+
+  const login = details => {
+    
+    if(details.email == adminUser.email && details.password == adminUser.password){
+      setUser({
+        name:details.name,
+        email:details.email,
+        password:details.password
+      })
+    }else{
+      setError("E-mail o password errata")
+    }
+
+  }
+
+  const logout = () =>{
+    setUser({
+      name:"",
+      email:""
+    })
+
+    
+  }
+  
+  // const [datiJson , setDatiJson] = useState ({})
+
+  // useEffect(() => {
+  //   const getDati = async() =>{
+  //     const dati = await fetch(api).then(res => res.json())
+  //     // return setDatiJson(dati.results.data)
+  //     return console.log(dati.results.data)
+  //   }
+  //   getDati()
+  // }, [api])
+ 
+  // fetch(api)
+  // .then(response => response.json())
+  // .then(data => console.log(data));
+  
+
+// const getData = () => {
+//     fetch(api)
+//       .then(res => res.json())
+//       .then(res => {
+//         console.log(res)
+//         return getData()
+//       })
+//       .catch(err => {})
+//   }
+
+
+// const fetchingData = async () =>{
+//   const res = await fetch(api)
+//   const data = await res.json()
+//   return data
+//   }
+  
+//   fetchingData()
+//   .then( data => console.log(data))
+//   .catch(error => console.log(error))
+
+
   return (
+    <>
+    <DataContextProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    {(user.email != "") ? (
+      <div className="logged">
+        <h2>Welcome,{user.name}</h2>
+        <button onClick={logout}>Logout</button>
+         <HeaderSito /> 
+      </div>
+    ) : (
+      <LoginForm Login={login} Error={error}/>
+    )}
     </div>
+    </DataContextProvider>
+
+    </>
   );
 }
 
